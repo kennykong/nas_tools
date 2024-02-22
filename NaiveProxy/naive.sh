e9d6460e7de37fdb7325507da0f8e2cdba5c5f96
Kenny <high0048@126.com>
2024-02-23 00:19:23 +0800

    Update naive.sh

--- NaiveProxy/naive.sh
+++ NaiveProxy/naive.sh
@@ -238,8 +238,8 @@ domain_check() {
 install_go() {
     cd /opt
     rm /opt/go*.tar.gz /usr/local/go/ -rf
-    wget https://go.dev/dl/go1.21.3.linux-${caddy_arch}.tar.gz
-    tar -zxf go1.21.3.linux-${caddy_arch}.tar.gz -C /usr/local/
+    wget https://go.dev/dl/go1.21.7.linux-${caddy_arch}.tar.gz
+    tar -zxf go1.21.7.linux-${caddy_arch}.tar.gz -C /usr/local/
     echo export GOROOT=/usr/local/go >> /etc/profile
     echo export PATH=$GOROOT/bin:$PATH >> /etc/profile
     source /etc/profile
