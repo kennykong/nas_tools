f8afdf72a49760102892ef8b020dd97ecac24628
Kenny <high0048@126.com>
2024-03-12 11:21:18 +0800

    Update naive.sh

--- NaiveProxy/naive.sh
+++ NaiveProxy/naive.sh
@@ -238,8 +238,8 @@ domain_check() {
 install_go() {
     cd /opt
     rm /opt/go*.tar.gz /usr/local/go/ -rf
-    wget https://go.dev/dl/go1.22.0.linux-${caddy_arch}.tar.gz
-    tar -zxf go1.22.0.linux-${caddy_arch}.tar.gz -C /usr/local/
+    wget https://go.dev/dl/go1.22.1.linux-${caddy_arch}.tar.gz
+    tar -zxf go1.22.1.linux-${caddy_arch}.tar.gz -C /usr/local/
     echo export GOROOT=/usr/local/go >> /etc/profile
     echo export PATH=$GOROOT/bin:$PATH >> /etc/profile
     source /etc/profile
@@ -257,7 +257,7 @@ install_caddy() {
     # download caddy file then install
     mkdir /root/src && cd /root/src/
     go install github.com/caddyserver/xcaddy/cmd/xcaddy@v0.3.5
-    ~/go/bin/xcaddy build --with github.com/caddyserver/forwardproxy@caddy2=github.com/klzgrad/forwardproxy@naive
+    ~/go/bin/xcaddy build --with github.com/caddyserver/forwardproxy@caddy2=github.com/klzgrad/forwardproxy@v2.7.6-naive
     cp caddy /usr/bin/
     /usr/bin/caddy version        # 2024-02-24 23:42
     #v2.7.6 h1:w0NymbG2m9PcvKWsrXO6EEkY9Ru4FJK8uQbYcev1p3A= 
